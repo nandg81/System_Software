82b8ab8b5ebce86b28bb9d1c36ee20197d5d3b28
Nandagopal N <nandg8@gmail.com>
2019-06-16 14:37:38 +0530

    Add files via upload

---
+++ 2PassMacroProcessor/mpass1 (3).c
@@ -0,0 +1,34 @@
+//Program to implement pass 1 of 2 pass macroprocessor
+//Author: Nandagopal
+
+#include<stdio.h>
+#include<string.h>
+
+main()
+{
+	FILE *ip,*op,*m;
+	char a[20],b[20],c[20];
+	ip=fopen("mip.txt","r");
+	op=fopen("dtab.txt","w");
+	m=fopen("m.txt","w");
+
+	while(!feof(ip))
+	{
+		fscanf(ip,"%s %s %s",a,b,c);	
+		if(strcmp(b,"MACRO")==0)
+		{
+			fprintf(op,"%s %s\n",a,c);
+			fprintf(m,"%s\n",a);
+			printf("\nMacro: %s",a);
+			while(strcmp(b,"MEND")!=0)
+			{
+				fscanf(ip,"%s %s %s",a,b,c);
+				fprintf(op,"%s %s\n",b,c);
+			}
+		}
+	}
+	printf("\nPass 1 completed");
+	fclose(ip);
+	fclose(op);
+	fclose(m);
+}	
